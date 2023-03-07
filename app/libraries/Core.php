eec83444f731452ed2af555a7adf696a2e6e98a7
gelug <gelu.grecu@gmail.com>
2023-03-07 17:19:03 +0200

    First step MVC

---
+++ app/libraries/Core.php
@@ -0,0 +1,14 @@
+<?php
+
+    class Core {
+        protected $currentContoller = 'Pages';
+        protected $currentMethode = 'index';
+        protected $params = [];
+
+        public function __construct() {
+            $this->getParams();
+        }
+        public function getParams() {
+            var_dump($_GET);
+        }
+    }

3adc33c92b4728f64ce860afb691cddfb03f8963
gelug <gelu.grecu@gmail.com>
2023-03-07 18:45:53 +0200

    added controllers

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -6,9 +6,23 @@
         protected $params = [];
 
         public function __construct() {
-            $this->getParams();
+           $url = $this->geUrl();
+           if($url){
+               if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')){
+                   $this->currentContoller = $url[0];
+                   unset($url[0]);
+               }
+           }
+           require_once('../app/controllers/' . $this->currentContoller . '.php');
+           $this->currentContoller = new $this->currentContoller;
         }
-        public function getParams() {
-            var_dump($_GET); 
+        public function geUrl() {
+            if (isset($_GET['url'])) {
+                $url = rtrim($_GET['url'], '/');
+                $url = filter_var($url, FILTER_SANITIZE_URL);
+                $url = explode('/', $url);
+                return $url;
+            }
+            return false;
         }
     }
